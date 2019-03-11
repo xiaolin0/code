28f8d9fadf094e3c8eabbea6948190f72832aa70
2889525019@qq.com <2889525019@qq.com>
2019-03-11 20:18:19 +0800

    3-11

--- Main.java
+++ Main.java
@@ -1,64 +1,59 @@
 import java.util.*;
 public class Main{
-	static int MAX=100000;   // .:路径    #:墙壁    S:起点   G:终点
-	static int n,m;
-	static int sx,sy;
-	static int ex,ey;
 	public static void main(String[] args){
-		Scanner scan=new Scanner(System.in);
-		n=scan.nextInt();
-		m=scan.nextInt();
-		char[][] ch=new char[n][m];
-		for(int i=0;i<n;i++){
-			String str=scan.next();
-			for(int j=0;j<m;j++){
-				ch[i][j]=str.charAt(j);
-				//寻找起点与终点
-				if(ch[i][j]=='S'){
-					sx=i;sy=j;
-				}else if(ch[i][j]=='G'){
-					ex=i;ey=j;
-				}
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt(); 
+		int a=5+n*4;
+		char[][] arr=new char[a][a];
+		for(int i=0;i<arr.length;i++){
+			for(int j=0;j<arr[i].length;j++){
+				arr[i][j]='.';
 			}
 		}
+		//首先将中心十字坐标计算出来,并存储$字符
+		int x=a/2;
+		int y=a/2;
+		for(int i=x-2;i<=x+2;i++){
+			arr[i][y]='$';
+			arr[x][i]='$';
+		}
+		//从内到外计算每一层并存储在字符数组中
+		for(int i=1;i<=n;i++){
+			//先存储四条边
+			int len=i*4+1; //每条边存储的个数
+			int tx=(5+n*4-len)/2;  //起点
+			for(int j=tx;j<len+tx;j++){
+				arr[x-2-i*2][j]='$';
+				arr[x+2+i*2][j]='$';
+				arr[j][x-2-i*2]='$';
+				arr[j][x+2+i*2]='$';
+			}
+			//存储四个内角
+			int jj=0;
+			for(int j=2+i*2-1;j>2*i-1;j--){
+				arr[x-j][tx]='$';
+				arr[x-j][arr[0].length-1-tx]='$';
+				arr[x+j][tx]='$';
+				arr[x+j][arr[0].length-1-tx]='$';
+				jj=j;
+			}
+			tx--;
+			arr[x-jj][tx]='$';
+			arr[x-jj][arr[0].length-1-tx]='$';
+			arr[x+jj][tx]='$';
+			arr[x+jj][arr[0].length-1-tx]='$';
+			prin(arr);
+		}
+		System.out.println("结束");
+		prin(arr);
 		
-		System.out.println(bfs(ch));
 	}
-	public static int bfs(char[][] ch){
-		Queue<int[]> que=new LinkedList();
-		int[][] help=new int[n][m];  //构建辅助数组，存储可以到达的点的步数
-		//存储四个方向
-		int[][] xx=new int[][]{{-1,0},
-				               {0,1},
-				               {1,0},
-				               {0,-1}
-		};
-		for(int i=0;i<n;i++)
-			for(int j=0;j<m;j++)help[i][j]=MAX;
-		que.offer(new int[]{sx,sy});  //压入起点
-		help[sx][sy]=0;
-		while(que.size()!=0){
-			int[] poll=que.poll();
-			if(poll[0]==ex && poll[1]==ey)break;
-			for(int i=0;i<4;i++){
-				int x=poll[0]+xx[i][0];
-				int y=poll[1]+xx[i][1];
-				if(x>=0 && x<n &&y>=0 && y<m && help[x][y]==MAX && ch[x][y]!='#'){
-					que.offer(new int[]{x,y});
-					help[x][y]=help[poll[0]][poll[1]]+1;
-				}
-			}
-//			prin(help);//查看每次行走的路径
-		}
-		return help[ex][ey];
-	}
-	public static void prin(int[][] str){//用于查看路径
-		for(int i=0;i<str.length;i++){
-			for(int j=0;j<str[i].length;j++){
-				System.out.printf("%2d ",str[i][j]);
+	public static void prin(char[][] ch){
+		for(int i=0;i<ch.length;i++){
+			for(int j=0;j<ch[i].length;j++){
+				System.out.print(ch[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("输出结束");
 	}
 }
